57197a19b7089f7056edf2d24ddfd18a23829466
leroux <leroux@babylonstoren.com>
2022-01-31 06:13:37 +0200

    second build

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>lerouxvanas</h1>
+                <h1>leroux/van/as</h1>
             </header>
         </div>
     );
