39197df6c37fce468e30f7d19e393d5312e4c279
leroux <leroux@babylonstoren.com>
2022-02-19 16:26:50 +0200

    style update

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import './App.css';
 function App() {
     return (
         <div className="App">
+            <div className="reg tl" />
+            <div className="reg tr" />
+            <div className="reg bl" />
+            <div className="reg br" />
             <header className="App-header">
-                <h1>leroux/van/as</h1>
+                <h1>Leroux van As</h1>
             </header>
         </div>
     );
